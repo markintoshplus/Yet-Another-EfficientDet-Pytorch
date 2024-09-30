a0fdec2f3ef7b27a5f6ce106bb287832d95550ca
markintoshplus <devera.markcedrick@gmail.com>
2024-09-30 19:25:25 +0800

    Fix COCO annotation file loading error

--- efficientdet/dataset.py
+++ efficientdet/dataset.py
@@ -14,7 +14,7 @@ class CocoDataset(Dataset):
         self.set_name = set
         self.transform = transform
 
-        self.coco = COCO(os.path.join(self.root_dir, 'annotations', 'instances_' + self.set_name + '.json'))
+        self.coco = COCO(os.path.join(self.root_dir, 'annotations', self.set_name + '_annotations.coco.json'))
         self.image_ids = self.coco.getImgIds()
 
         self.load_classes()
